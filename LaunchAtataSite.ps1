cc2eefcef9aecb79f95d0badac5518e803510943
Melnyk Artemyj <melnykartemyj@gmail.com>
2018-09-10 15:59:53 +0300

    Update LaunchAtataSite.ps1

--- LaunchAtataSite.ps1
+++ LaunchAtataSite.ps1
@@ -1,17 +1,12 @@
-﻿$siteFolderPath = "c:\projects\myproject\src\Atata.TestApp"
-
-$port = 57612
-$servedAt = "http://localhost:$($port)/"
-write-host "jVUNDemo location: $siteFolderPath"
-
-write-host "Spin up jVUNDemo site at $($servedAt)"
-$process = Start-Process 'C:\Program Files (x86)\IIS Express\iisexpress.exe' -NoNewWindow -ArgumentList "/path:$($siteFolderPath) /port:$($port)"
-
-write-host "Wait a moment for IIS to startup"
-Start-sleep -s 15
-
-write-host "Create static version of demo site here: $($siteFolderPath)"
-Push-Location $staticSiteParentPath
+      $siteFolderPath = "c:\projects\myproject\src\Atata.TestApp"
+      $port = 50549
+      $servedAt = "http://localhost:$($port)/"
+      write-host "Atata demo location: $siteFolderPath"
+      write-host "Spin up Atata site at $($servedAt)"
+      $process = Start-Process 'C:\Program Files (x86)\IIS Express\iisexpress.exe' -NoNewWindow -ArgumentList "/path:$($siteFolderPath) /port:$($port)"
+      write-host "Wait a moment for IIS to startup"
+      Start-sleep -s 15
+      write-host "Create static version of demo site here: $($siteFolderPath)"
 
 
 
